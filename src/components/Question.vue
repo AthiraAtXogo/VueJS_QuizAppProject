33a63124c0bf32bae87ed0ba54061630c9bafe0d
AthiraAtXogo <athiraatxogo@gmail.com>
2024-07-31 15:48:02 +0530

    App.vue , main.js , Question , Result  file commited

---
+++ src/components/Question.vue
@@ -0,0 +1,53 @@
+<!-- <template>
+    <div class=" relative w-full">
+        <div class="h-[3.125rem] mt-[.625rem] bg-[#ddd] relative">
+            <div class="h-[3.125rem] bg-[#ff6372] transition-all duration-[0.3s] ease-linear"></div>
+            <div class="absolute top-[.9375rem] left-0 text-center text-[#fff] w-full">1 out of 3 questions answered</div>
+        </div>
+        <div class="relative w-full" v-for="(question,qIndex) in questions" :key="question.q" v-show="questionAnswered==qIndex">
+           
+            <div class=" w-full p-5 text-[2rem] font-bold text-center bg-[#00ca8c] text-[#fff] box-border ">{{question.q}}</div>
+           
+            <div class="border-solid border border-[#8e959f] p-5 text-[18px] w-full bg-[#fff] transition-all duration-[0.2s] ease-linear ">
+                <div class="border-solid border border-[#8e959f] p-5 text-[18px] w-full bg-[#fff] transition-all duration-[0.2s] ease-linear cursor-pointer hover:bg-[#8ce200] hover:border-[#8ce200] hover:text-[#fff] "  v-for="answer in question.answers" 
+                :key="answer.text" 
+                @click.prevent="selectAnswer(answer.is_correct)">
+                {{ answer.text }}</div>
+                
+            </div>
+        </div>
+    </div>
+</template> -->
+
+<template>
+    <div class="relative w-full">
+        <div class="h-[3.125rem] mt-[.625rem] bg-[#ddd] relative">
+           
+            <div class="h-[3.125rem] bg-[#ff6372] transition-all duration-[0.3s] ease-linear" :style="{width:`${(questionAnswer/questions.length)*100}%`}"></div>
+            
+            <div class="absolute top-[.9375rem] left-0 text-center text-[#fff] w-full">{{ questionAnswer }} out of {{ questions.length }} questions answered</div>
+        </div>
+        <div class="relative w-full" v-for="(question, qIndex) in questions" :key="question.q" v-show="questionAnswer === qIndex">
+            <div class="w-full p-5 text-[2rem] font-bold text-center bg-[#00ca8c] text-[#fff] box-border">{{ question.q }}</div>
+           
+            <div  class="cursor-pointer">
+                <div class="border-solid border border-[#8e959f] p-5 text-[18px] w-full bg-[#fff] transition-all duration-[0.2s] ease-linear hover:bg-[#8ce200] hover:border-[#8ce200] hover:text-[#fff]" v-for="answer in question.answers" :key="answer.text" @click.prevent="selectAnswer(answer.is_correct)">{{ answer.text }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"Question",
+    props:['questions' , 'questionAnswer'],
+    emits:['question-answered'],
+    methods:{
+        selectAnswer(is_correct){
+            this.$emit('question-answered',is_correct)
+        }
+    }
+}
+
+</script>
+
---
+++ src/components/Result.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="w-full">
+        <div class="w-full p-[20px] text-[32px] font-bold text-center bg-[#12cbc4] text-[#fff] box-border">
+            {{ results[resultIndex].title }}</div>
+        <div class="border border-solid border-[#8e959f] p-[20px] text-[18px] w-full bg-[#fff] transition-all duration-[0.4s] ease-linear text-center">
+            {{ results[resultIndex].desc }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"Result",
+    props:['results',"totalCorrect"],
+    computed:{
+       resultIndex(){
+        let index = 0;
+
+        this.results.forEach((e ,i) => {
+            if(e.min <= this.totalCorrect && e.max >= this.totalCorrect){
+               index= i; 
+            }
+        });
+
+        return index;
+       } 
+    }
+}
+
+</script>
+
